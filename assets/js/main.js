6168607a4693b27d2a5c3a57d2b32f0becc96787
vanita22 <inesvanessapcm@gmail.com>
2017-05-09 13:43:50 -0300

    algunos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,27 +19,21 @@ const vane = new Student("Vane", 80, 80, "Active");
 
 var listaStudent = [];
 listaStudent.push(su, pola, nat, vane);
-console.log(listaStudent);
 
-function agregarEstudiante() {
-    alert('funciona agregarEstudiante');
-    var valorStudent = document.getElementById("student").value;
-    student.push(new Tarea(1, 20, valorStudent, false));
-    document.getElementById("student").value = "";
-    return student;
-}
+
+
 
 //imprime los datos una vez que se presione el boton
 
-function mostrarEstudiante() {
-    alert('funciona mostrarEstudiante');
-    printHTML(listaStudent)
-    student.forEach(function(elemento) {
-        imprimir.innerHTML += "<li>" + "nombre:" + elemento.nombre + "\n Tech Skills: " + elemento.techSkills + "\n Life Skills: " + elemento.lifeSkills + "\n stats: " + elemento.stats + "</li><input type='button' onclick='mostrarStudent' value='student' id='student'" + "<br><br>"
+
+ var estudiante =  listaStudent.forEach(function(elemento) {
+        elemento.innerHTML += "<li>" + "nombre:" + this.nombre + "Tech Skills: " + this.techSkills + "Life Skills: " + this.lifeSkills + "stats: " + this.stats + "</li>" + "<br><br>"
     });
-}
+
+
+estudiante();
 
 function printHTML(html) {
-    imprimir.innerHTML = '';
-    imprimir.innerHTML = html;
+    elemento.innerHTML = '';
+    elemento.innerHTML = html;
 }
