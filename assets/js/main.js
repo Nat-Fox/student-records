77aa012c2790dcf50b861dea703a4ebe3a7721f3
Natalia Garrido <natgramirez@outlook.com>
2017-05-09 12:47:04 -0300

    corrigiendo problemas de llaves

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function Student (nombre, techSkills, lifeSkills, stats) {
+function Student(nombre, techSkills, lifeSkills, stats) {
     this.nombre = nombre;
     this.techSkills = techSkills;
     this.lifeSkills = lifeSkills;
@@ -6,10 +6,7 @@ function Student (nombre, techSkills, lifeSkills, stats) {
 
 };
 
-const su = new  Student( "Su", 80, 80,"Active")
-const pola = new Student("Pola",85, 85, "Active")
-const nat = new Student("Nath", 90, 90, "Active")
-const vane = new Student("Vane", 80, 80, "Active")
-
-};
-
+const su = new Student("Su", 80, 80, "Active");
+const pola = new Student("Pola", 85, 85, "Active");
+const nat = new Student("Nath", 90, 90, "Active");
+const vane = new Student("Vane", 80, 80, "Active");
