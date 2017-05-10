c3a99793fa0d19ab058d0ca03c2edb99d12ec8c5
vanita22 <vanitayrafita2004@gmail.com>
2017-05-10 08:34:17 -0300

    subiendo otra vez mi parte con los cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,26 +16,47 @@ const vane = new Student("Vane", 80, 80, "Active");
 
 
 var listaStudent = [];
-listaStudent.push(su, pola, nat, vane);
-
-
-
 
 //función para agregar un estudiante
 function agregarEstudiante() {
     //pedimos los datos por prompt
-    var nombre = prompt('Ingresa nombre de la estudiante');
+do{
+    var nombre = prompt('Introduce tu nombre');   
     var techSkills = prompt('Ingresa tech skills de la estudiante');
     var lifeSkills = prompt('Ingresa life skills de la estudiante');
     var stats = prompt('Ingresa status de la estudiante');
+
+    
+} while(nombre== " " || techSkills== " " || lifeSkills== " " || stats == " "){
+    alert('ingrese campo');
+   
+}
+  
     //creamos a cada estudiante en base al objeto
     var estudiante = new Student(nombre, techSkills, lifeSkills, stats);
+    
+
     //estudiante ingresado al arreglo listaStudent
     listaStudent.push(estudiante);
     console.log(listaStudent);
 }
-
+  
 
 //funcion que imprime los datos una vez que se presione el boton
 
+function mostrarEstudiante() {
+    var contenedor = document.getElementById('mostrar');
+    var contenido = '';
+
+    //recorremos el arreglo listaStudent
+    listaStudent.forEach(function(student) {
+        contenido += '<div>'+ '<font color= #FFFFFF>' + '<strong>' + student.nombre.toUpperCase(0).charAt(0) + student.nombre.slice(1).toLowerCase() + '</strong>' + '<br>' +
+            '<strong>' + 'Tech Skills:' + '</strong>' + student.techSkills + '<br>' +
+            '<strong>' + 'Life Skills:' + '</strong>' + student.lifeSkills + '<br>' +
+            '<strong>' + 'Status' + '</strong>' + student.stats + '</font>'+ '</div><br>';
+        console.log(contenido);
+    });
+
+    contenedor.innerHTML = contenido;
+}
 
